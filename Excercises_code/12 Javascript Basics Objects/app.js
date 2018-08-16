8d75b8029e904f5c16daf06aa2c008238064b5bb
Oleg Strigo <mrstriga@gmail.com>
2018-08-16 14:49:15 +0300

    Finish

--- Excercises_code/12 Javascript Basics Objects/app.js
+++ Excercises_code/12 Javascript Basics Objects/app.js
@@ -1,1 +1,37 @@
-alert()
+const movies = [
+	{
+		title: 'In bruges',
+		hasWathced: true,
+		rating: 5
+	},
+	{
+		title: 'Frozen',
+		hasWathced: false,
+		rating: 4.5
+	},
+	{
+		title: 'Mad Max Fury Road',
+		hasWathced: true,
+		rating: 5
+	},
+	{
+		title: 'Les Miserables',
+		hasWathced: false,
+		rating: 3.5
+	},
+]
+
+function buildString(m) {
+	let result = 'You have ';
+	if (m.hasWathced) {
+		result += 'watched ';
+	} else {
+		result += 'not seen ';
+	}
+	result += `"${m.title}" - ${m.rating} stars`;
+	return result;
+}
+
+movies.forEach(function (movie) {
+	console.log(buildString(movie));
+});
